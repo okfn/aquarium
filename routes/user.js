13744cea7333e2b2cafa16f2b61f05b1e0515087
Rowan Crawford <wombleton@gmail.com>
2013-09-18 13:22:09 +1200

    If no users, redirect to setup; if one user, redirect to login

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,32 @@
-/*
- * GET users listing.
-*/
+var db = require('../lib/db');
 
-exports.list = function(req, res){
-    res.send("respond with a resource");
+/**
+ * Show the login page. Redirects to /setup if no users at all.
+ */
+exports.login = function(req, res) {
+    var accounts = db.coll("accounts");
+
+    accounts.count(function(err, count) {
+        if (count === 0) {
+            res.redirect('/setup');
+        } else {
+            res.render('login', {
+                title: 'Login'
+            });
+        }
+    });
 };
+
+exports.setup = function(req, res) {
+    var accounts = db.coll("accounts");
+
+    accounts.count(function(err, count) {
+        if (count === 0) {
+            res.render('setup', {
+                title: 'Admin User Setup'
+            });
+        } else {
+            res.redirect('/login');
+        }
+    });
+}
