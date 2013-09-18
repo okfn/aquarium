35c964966db47b52b1a4ac2b527c854e8d8b84d3
Rowan Crawford <wombleton@gmail.com>
2013-09-18 18:43:42 +1200

    Use passport-local strategy; serialize/deserialize user

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 */
 
 var express = require('express'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     routes = require('./routes'),
     user = require('./routes/user'),
     http = require('http'),
@@ -19,7 +21,11 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({
+    secret: 'session secret'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,10 +36,40 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/login', user.login);
-app.get('/setup', user.setup);
+app.get('/login', user.showLogin);
+app.get('/setup', user.showSetup);
+app.post('/setup', user.createAdmin);
 
 db.init(function() {
+    var users = db.coll('users');
+
+    passport.use(new LocalStrategy(function(username, password, done) {
+        users.findOne(username, function(err, user) {
+            if (err) {
+                done(err);
+            } else if (user) {
+                bcrypt.compare(password, user.hash, function(err, matches) {
+                    if (err || !matches) {
+                        done(err, false, { message: 'Incorrect password.' });
+                    } else {
+                        done(null, user);
+                    }
+                });
+            } else {
+                done(null, false, { message: 'Incorrect username.' });
+            }
+        });
+    }));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        users.findOne(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
     http.createServer(app).listen(app.get('port'), function() {
         console.log('Express server listening on port ' + app.get('port'));
     });
