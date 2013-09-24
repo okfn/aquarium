56c74d9bc770d13c71f389c5c6f3d833aa42c3bf
Rowan Crawford <wombleton@gmail.com>
2013-09-24 12:26:42 +1200

    Create new docs

--- routes/documents.js
+++ routes/documents.js
@@ -7,6 +7,8 @@ module.exports = {
     init: function(app) {
         app.get('/documents', isAuthenticated, module.exports.showDocs);
         app.get('/documents/new', isAuthenticated, module.exports.newDoc);
+
+        app.post('/documents', isAuthenticated, module.exports.createDoc);
     },
     showDocs: function(req, res) {
         docs.list({
@@ -24,6 +26,33 @@ module.exports = {
     newDoc: function(req, res) {
         res.render('newdoc', {
             title: 'New Document'
+        });
+    },
+    createDoc: function(req, res) {
+        var options = {
+            username: req.user.username,
+            data: {
+                type: req.body.type,
+                title: req.body.title,
+                available: req.body.available === 'yes',
+                location: {
+                    category: req.body.location,
+                    detail: req.body.location_detail
+                },
+                url: req.body.url,
+                date_published: req.body.date_published,
+                date_received: req.body.date_received,
+                softcopy: req.body.softcopy === 'yes',
+                scanned: req.body.scanned === 'yes'
+            }
+        };
+
+        docs.insert(options, function(err) {
+            if (err) {
+                return janitor.error(res, err);
+            }
+
+            res.redirect('/documents');
         });
     }
 };
