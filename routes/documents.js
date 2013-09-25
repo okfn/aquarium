a271d93a6fb22653212381f47203080ecce1889e
Rowan Crawford <wombleton@gmail.com>
2013-09-25 16:01:07 +1200

    Implement updating documents

--- routes/documents.js
+++ routes/documents.js
@@ -7,8 +7,10 @@ module.exports = {
     init: function(app) {
         app.get('/documents', isAuthenticated, module.exports.showDocs);
         app.get('/documents/new', isAuthenticated, module.exports.newDoc);
+        app.get('/documents/:id', isAuthenticated, module.exports.showDoc);
 
         app.post('/documents', isAuthenticated, module.exports.createDoc);
+        app.post('/documents/:id', isAuthenticated, module.exports.updateDoc);
     },
     showDocs: function(req, res) {
         docs.list({
@@ -24,6 +26,21 @@ module.exports = {
             });
         });
     },
+    showDoc: function(req, res) {
+        docs.get({
+            id: req.params.id
+        }, function(err, doc) {
+            if (err) {
+                return janitor.error(res, err);
+            } else if (!doc) {
+                return janitor.missing(res);
+            }
+            res.render('document', {
+                doc: doc,
+                title: doc.title
+            });
+        });
+    },
     newDoc: function(req, res) {
         res.render('newdoc', {
             title: 'New Document'
@@ -32,20 +49,7 @@ module.exports = {
     createDoc: function(req, res) {
         var options = {
             username: req.user.username,
-            data: {
-                type: req.body.type,
-                title: req.body.title,
-                available: req.body.available === 'yes',
-                location: {
-                    category: req.body.location,
-                    detail: req.body.location_detail
-                },
-                url: req.body.url,
-                date_published: req.body.date_published,
-                date_received: req.body.date_received,
-                softcopy: req.body.softcopy === 'yes',
-                scanned: req.body.scanned === 'yes'
-            }
+            data: extractDoc(req)
         };
 
         docs.insert(options, function(err) {
@@ -55,5 +59,48 @@ module.exports = {
 
             res.redirect('/documents');
         });
+    },
+    updateDoc: function(req, res) {
+        var id = req.params.id;
+
+        docs.get({
+            id: id
+        }, function(err, doc) {
+            if (err || !doc) {
+                return janitor.error(res, err || 'Invalid document.');
+            } else if (!req.user.admin && doc.username !== req.user.username) {
+                return janitor.error(res, "Unable to update document.");
+            }
+
+        debugger;
+
+            docs.update({
+                data: extractDoc(req),
+                id: id
+            }, function(err) {
+                if (err) {
+                    return janitor.error(res, err);
+                }
+
+                res.redirect('/documents/' + id);
+            });
+        });
     }
 };
+
+function extractDoc(req) {
+    return {
+        type: req.body.type,
+        title: req.body.title,
+        available: req.body.available === 'yes',
+        location: {
+            category: req.body.location,
+            detail: req.body.location_detail
+        },
+        url: req.body.url,
+        date_published: req.body.date_published,
+        date_received: req.body.date_received,
+        softcopy: req.body.softcopy === 'yes',
+        scanned: req.body.scanned === 'yes'
+    };
+}
