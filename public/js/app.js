9a76be557b9e69928841983de39c1e1f1a16eeea
Rowan Crawford <wombleton@gmail.com>
2013-10-03 16:29:40 +1300

    Use browserify for validator

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+var $ = require('./jquery-2.0.3.min'),
+    bootstrap = require('./bootstrap'),
+    validator = require('./validator-min');
+
+$(document).ready(function() {
+    console.log('what');
+});
