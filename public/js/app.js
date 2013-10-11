e986dbd6d0ba77003e06557276da3e45e98b5ca0
Rowan Crawford <wombleton@gmail.com>
2013-10-11 13:39:00 +1300

    Add validation to frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,72 @@
 var $ = require('./jquery-2.0.3.min'),
-    bootstrap = require('./bootstrap'),
-    validator = require('./validator-min');
+    serializer = require('serialize-form').serializeFormObject,
+    Validator = require('./validator-min').Validator;
 
-$(document).ready(function() {
-    console.log('what');
+require('./jquery-fns');
+require('./bootstrap');
+
+Validator.prototype.error = function (msg) {
+    this._errors.push(msg);
+    return this;
+};
+
+Validator.prototype.getErrors = function () {
+    return this._errors;
+};
+
+$(document).on('input', 'form#login', function() {
+    var $this = $(this),
+        values = serializer('form#login'),
+        errors,
+        validator;
+
+    validator = new Validator();
+    validator.check(values.username, 'Username must be an email.').isEmail();
+    validator.check(values.password, 'Password must be 8 characters or more.').len(8);
+
+    errors = validator.getErrors();
+
+    if (errors.length) {
+        $this.find('[type=submit]').disable();
+    } else {
+        $this.find('[type=submit]').enable();
+    }
+});
+
+$(document).on('input', 'form#setup', function() {
+    var $this = $(this),
+        values = serializer('form#setup'),
+        errors,
+        validator;
+
+    validator = new Validator();
+    validator.check(values.username, 'Username must be an email.').isEmail();
+    validator.check(values.password, 'Password must be 8 characters or more.').len(8);
+    validator.check(values.confirm, 'Passwords must match.').same(values.password);
+
+    errors = validator.getErrors();
+
+    if (errors.length) {
+        $this.find('[type=submit]').disable();
+    } else {
+        $this.find('[type=submit]').enable();
+    }
+});
+
+$(document).on('input', 'form#newdoc, form#updatedoc', function() {
+    var $this = $(this),
+        values = serializer('form#newdoc, form#updatedoc'),
+        errors,
+        validator;
+
+    validator = new Validator();
+    validator.check(values.title, 'Document must have a title.').notEmpty();
+
+    errors = validator.getErrors();
+
+    if (errors.length) {
+        $this.find('[type=submit]').disable();
+    } else {
+        $this.find('[type=submit]').enable();
+    }
 });
