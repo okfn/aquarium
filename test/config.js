0ceffb2daa86024530786d0b8aefcf32a512685a
Vitor Baptista <vitor@vitorbaptista.com>
2014-07-25 11:32:40 -0300

    Fix tests

--- test/config.js
+++ test/config.js
@@ -4,12 +4,14 @@ exports['invalid if nothing'] = function(test) {
     test.equals(config.valid({}), false);
 
     test.done();
-}
+};
 
 exports['valid if all supplied'] = function(test) {
     test.equals(config.valid({
-        DB_PATH: 'x'
+        DB_PATH: 'x',
+        COOKIE_SECRET: 'x',
+        SESSION_SECRET: 'x'
     }), true);
 
     test.done();
-}
+};
