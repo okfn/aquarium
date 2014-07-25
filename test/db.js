8c45cec602789f5e7df7a1c35840aefa8640c3b0
Vitor Baptista <vitor@vitorbaptista.com>
2014-07-25 13:29:59 -0300

    Fix db tests
    They were passing because they Mocha wasn't waiting for the callbacks to run.

--- test/db.js
+++ test/db.js
@@ -5,19 +5,21 @@ var assert = require('assert'),
 
 describe('db', function() {
   describe('#init', function() {
-    it('should succeed when run with the configured mongo', function() {
+    it('should succeed when run with the configured mongo', function(done) {
       db.init(function(err) {
           assert.equal(err, undefined);
           db.close();
+          done();
       });
     });
 
-    it('should fail if its invalid', function() {
+    it('should fail if its invalid', function(done) {
       sinon.stub(config, 'valid').returns(false);
 
       db.init(function(err) {
           assert.equal(err, "Set valid DB variables in the .env file.");
           config.valid.restore();
+          done();
       });
     });
   });
