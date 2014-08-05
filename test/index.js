302f580a55d235e4d7516b5cc5d66edf98b3ab89
Vitor Baptista <vitor@vitorbaptista.com>
2014-08-05 16:03:30 -0300

    Adds countries.insert and tests for it and countries.list

--- test/index.js
+++ test/index.js
@@ -1,7 +1,19 @@
 var assert = require('assert'),
-    index = require('../index');
+    config = require('../lib/config'),
+    original_DB_PATH = config.DB_PATH,
+    TEST_DB_PATH = original_DB_PATH + '_test';
+
+before(function() {
+  config.DB_PATH = TEST_DB_PATH;
+});
+
+after(function() {
+  config.DB_PATH = original_DB_PATH;
+});
 
 describe('index', function() {
+  var index = require('../index');
+
   it('should export the app', function() {
     assert.notEqual(index.app, undefined);
   });
---
+++ test/lib/countries.js
@@ -0,0 +1,65 @@
+var assert = require('assert'),
+    db = require('../../lib/db'),
+    countries = require('../../lib/countries');
+
+describe('countries', function() {
+  before(function(done) {
+    db.init(done);
+  });
+
+  describe('#list', function() {
+    beforeEach(function(done) {
+      db.dropDatabase(done);
+    });
+
+    it('should return an empty list if there\'re no countries', function(done) {
+      countries.list(function (err, countries) {
+        assert.equal(err, undefined);
+        assert.deepEqual(countries, []);
+        done();
+      });
+    });
+
+    it('should return the country name and its latest OBI score', function(done) {
+      var data = {
+        country: 'Brazil',
+        obi_scores: [{ score: 39, year: 2013 }, { score: 42, year: 2014 }]
+      };
+
+      countries.insert(data, function (err) {
+        assert.equal(err, undefined);
+        countries.list(function (err, countries) {
+          var expected = [{
+            country: data.country,
+            obi_score: data.obi_scores[1].score,
+            obi_year: data.obi_scores[1].year,
+          }];
+
+          assert.equal(err, undefined);
+          assert.deepEqual(countries, expected);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#insert', function() {
+    beforeEach(function(done) {
+      db.dropDatabase(done);
+    });
+
+    it('should work', function(done) {
+      var data = { country: 'Brazil' };
+
+      countries.insert(data, function (err) {
+        assert.equal(err, undefined);
+        countries.list(function (err, countries) {
+          assert.equal(err, undefined);
+          assert.equal(countries.length, 1);
+          assert.equal(countries[0].country, 'Brazil');
+          done();
+        });
+      });
+    });
+  });
+});
