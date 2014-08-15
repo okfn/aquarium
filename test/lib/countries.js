ff1c2ed1f8e2513ec4669de75e7c0b83ece79071
Vitor Baptista <vitor@vitorbaptista.com>
2014-08-15 16:33:38 -0300

    [#69] Add document info on country endpoint

--- test/lib/countries.js
+++ test/lib/countries.js
@@ -1,6 +1,9 @@
 var assert = require('assert'),
+    async = require('async'),
+    moment = require('moment'),
     db = require('../../lib/db'),
-    countries = require('../../lib/countries');
+    countries = require('../../lib/countries'),
+    users = require('../../lib/users');
 
 describe('countries', function() {
   before(function(done) {
@@ -83,7 +86,67 @@ describe('countries', function() {
         assert.ifError(err);
         countries.get({ code: 'BR' }, function (err, country) {
           assert.ifError(err);
+          delete country.documents;
           assert.deepEqual(country, data);
+          done();
+        });
+      });
+    });
+
+    it('should return null if couldn\'t find country code', function(done) {
+      countries.get({ code: 'inexistent-code' }, function (err, country) {
+        assert.ifError(err);
+        assert.equal(country, null);
+        done();
+      });
+    });
+
+    it('should include the sites as documents', function(done) {
+      var country = { country: 'Brazil', code: 'BR' },
+          user = {
+            username: 'username',
+            password: 'password',
+            user: {
+              country: country.code + ' - ' + country.country,
+              admin: false,
+              sites: [{
+                active: true,
+                title: 'the title',
+                type: 'Audit Report',
+                search_dates: {
+                  start: moment('2014-01-01').toString(),
+                  end: moment('2014-06-01').toString(),
+                }
+              }],
+            },
+          };
+
+      var expectedCountry = {
+        country: country.country,
+        code: country.code,
+        documents: [{
+          title: user.user.sites[0].title,
+          type: user.user.sites[0].type,
+          expected_date: user.user.sites[0].search_dates.start,
+          state: 'late',
+        }],
+      };
+
+      async.parallel({
+        country: function(callback) {
+          countries.insert(country, callback);
+        },
+        user: function(callback) {
+          users.drop();
+          users.insert(user, callback);
+        },
+      }, function(err, results) {
+        assert.ifError(err);
+        countries.get({ code: country.code }, function (err, country) {
+          assert.ifError(err);
+          delete country._id;
+          assert.deepEqual(country, expectedCountry);
+          users.drop();
           done();
         });
       });
