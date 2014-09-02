96fa93b7e2d7670ceae6da4f783925dce3194501
Vitor Baptista <vitor@vitorbaptista.com>
2014-09-02 00:01:31 -0300

    [#72] Adds "Expected Date of Publication" to docs

--- test/lib/documents.js
+++ test/lib/documents.js
@@ -14,20 +14,26 @@ describe('documents', function() {
 
     it('should parse the dates correctly', function() {
       var doc,
+          expectedDatePublished = '22 Abr 2014',
           datePublished = '07 Sep 2014',
           dateReceived = '09 Oct 2014',
           dateFormat = 'DD MMM YYYY',
           body = {
             type: 'Pre-Budget Statement',
             title: '2014 Budget',
+            expected_date_published: expectedDatePublished,
             date_published: datePublished,
             date_received: dateReceived,
           };
 
       doc = documents.validateAndBuild(body, assertStub);
 
-      assert.equal(doc.date_published._i, moment(datePublished, dateFormat)._i);
-      assert.equal(doc.date_received._i, moment(dateReceived, dateFormat)._i);
+      assert.equal(doc.expected_date_published._i,
+                   moment(expectedDatePublished, dateFormat)._i);
+      assert.equal(doc.date_published._i,
+                   moment(datePublished, dateFormat)._i);
+      assert.equal(doc.date_received._i,
+                   moment(dateReceived, dateFormat)._i);
     });
   });
 });
